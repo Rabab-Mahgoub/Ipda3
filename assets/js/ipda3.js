8a91de3b8ac3e6a868b2acbc09b77bc685dddfbf
Rabab-Mahgoub <rababmahgoub5@gmail.com>
2021-10-20 19:34:48 +0200

    first commit

--- assets/js/ipda3.js
+++ assets/js/ipda3.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var one = $("#one");
     var two = $("#two");
+    var sliider =$('#slide');
  
     one.owlCarousel({
     loop:true,
@@ -47,7 +48,37 @@ $(document).ready(function(){
         }
         }
         );
-      
+        
+        sliider.owlCarousel({
+         loop:true,
+         margin:5,
+         rtl:true,
+         center: true,
+         nav:true,
+         dots:false,
+         navText: [
+             "<span style(background-color: violet)><i class='fa fa-angle-left'></i></span>",
+             "<i class='fa fa-angle-right'></i>"
+         ],
+         responsiveClass:true,
+         responsive:{
+             0:{
+                 items:1,
+                 nav:true
+             },
+             600:{
+                 items:2,
+                 nav:true
+             },
+             1000:{
+                 items:3,
+                 nav:true,
+                 loop:true
+             }
+         }
+         }
+         );
+
      
 
 
